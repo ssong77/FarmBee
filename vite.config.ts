6c3e6ecfda6ba6c1fa86bf708d8298451bcde8b4
ssong77 <sksmswlsdn1@gmail.com>
2025-05-28 16:07:12 +0900

    라우팅 구조 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // dev 모드일 땐 기본 경로, prod 모드일 땐 깃허브 Pages 리포지토리명
   return {
-    base: mode === 'development' ? '/' : '/FarmBee/', // 저장소 이름에 맞게 수정
+    base: '/FarmBee/', // 저장소 이름에 맞게 수정
     plugins: [react()],
   }
 })
